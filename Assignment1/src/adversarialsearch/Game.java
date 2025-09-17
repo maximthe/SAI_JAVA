92126ed7894d92bef56ba16606df17b7580bbf91
Maksim Rogov <maximrogov0409@gmail.com>
2025-09-17 16:36:21 +0200

    Maybe working minimax

--- Assignment1/src/adversarialsearch/Game.java
+++ Assignment1/src/adversarialsearch/Game.java
@@ -10,74 +10,54 @@ public class Game {
 	}
 	public void test() {
 		
-		System.out.println(minimax(b, b.turn, 20, 0).value(b.turn));
-		
-		while (!b.isLeaf()){
-			System.out.println(b.toString());
-			System.out.println("Legal moves for agent with turn:"+b.legalMoves());
-			b.execute(b.legalMoves().get((int)(Math.random()*b.legalMoves().size())));
-		}
+		System.out.println(minimax(b, b.turn, 13, 0).value(b.turn));
+		System.out.println(minimax(b, b.turn, 13, 0));
+		System.out.println(minimax(b, b.turn, 13, 0).score[0]);
+		System.out.println(minimax(b, b.turn, 13, 0).score[1]);
 	}
 	
 	public State minimax(State s, int forAgent, int maxDepth, int depth) {
-		// make a variable to keep track of the state we return
-		State tempState = s;
 		
-		System.out.println(depth);
-		
-		// if we hit the depth limit, return the state without further checking
-		if (depth >= maxDepth) {
-			return s;
-		}
-		
-		// if we are in a leaf node, return the state
-		if (s.isLeaf()) {
-			return s;
-		}
-		
-		if (s.turn == forAgent) {
-			// if it is our turn, go through our moves and select the max
-			double temp = -999;
-			
-			for (String move: s.legalMoves()) {
-				
-				// make a new branch
-				State nextState = s.copy();
-				nextState.execute(move);
-				
-				// find if the branch leads to a better value for the agent
-				double branchValue = minimax(nextState, 1 - forAgent, maxDepth, depth++).value(1 - forAgent);
-				if (branchValue > temp) {
-					
-					// update the temporary best value and state
-					temp = branchValue;
-					tempState = nextState;
-				}
-			}
-			
-		} else {
-			
-			// if it is NOT our turn, go through our moves and select the max
-			double temp = 999;
-			
-			for (String move: s.legalMoves()) {
-				
-				// make a new branch
-				State nextState = s.copy();
-				nextState.execute(move);
-				
-				// find if the branch leads to a better value for the opponent
-				double branchValue = minimax(nextState, 1 - forAgent, maxDepth, depth++).value(1 - forAgent);
-				if (branchValue < temp) {
-					
-					// update the temporary "worst" value and state
-					temp = branchValue;
-					tempState = nextState;
-				}
-			}
-			
-		}
-		
-		return tempState;
+	    // if we hit the depth limit or leaf node, return the state
+	    if (depth >= maxDepth || s.isLeaf()) {
+	        return s;
+	    }
+
+	    State bestState = null;
+
+	    if (s.turn == forAgent) {
+	        double bestValue = Double.NEGATIVE_INFINITY;
+
+	        for (String move : s.legalMoves()) {
+	            State nextState = s.copy();
+	            nextState.execute(move);
+
+	            State candidate = minimax(nextState, forAgent, maxDepth, depth + 1);
+	            double branchValue = candidate.value(forAgent);
+
+	            if (branchValue > bestValue) {
+	                bestValue = branchValue;
+	                bestState = candidate;
+	            }
+	        }
+	    } else {
+	        double bestValue = Double.POSITIVE_INFINITY;
+
+	        for (String move : s.legalMoves()) {
+	            State nextState = s.copy();
+	            nextState.execute(move);
+
+	            State candidate = minimax(nextState, forAgent, maxDepth, depth + 1);
+	            double branchValue = candidate.value(forAgent);
+
+	            if (branchValue < bestValue) {
+	                bestValue = branchValue;
+	                bestState = candidate;
+	            }
+	        }
+	    }
+
+	    return bestState;
 	}
+
 }
